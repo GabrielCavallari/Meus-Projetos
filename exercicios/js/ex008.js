fe7f383732ebce93d693bb82e64533b8c203271f
Gabriel Cavallari <gabriel.cavallari2003@gmail.com>
2024-03-03 20:06:30 -0300

    exercicios

---
+++ exercicios/js/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(` A velocidade do seu carro é ${vel}Km/h `)
+if(vel > 60){
+    console.log(`Velocidade ultrapassou o permitido. MULTADO!`)
+}
+console.log(`Dirija sempre com sinto de segurança`)
